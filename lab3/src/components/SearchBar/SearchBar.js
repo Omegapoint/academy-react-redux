58a0292c20b1a3dea3110fb15bea746bdd966f3c
Robert Novo <robert.j.novo@gmail.com>
2019-05-22 20:10:14 +0200

    formatted code

--- lab3/src/components/SearchBar/SearchBar.js
+++ lab3/src/components/SearchBar/SearchBar.js
@@ -1,23 +1,26 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+
 class SearchBar extends Component {
-    render() {
-        return (
-					<div className="input-group mb-12">
-						<div className="input-group-prepend">
-							<span className="input-group-text" id="basic-addon1" role="img" aria-label="search">
-								🔎
-							</span>
-						</div>
-						<input type="text"
-									 className="form-control"
-									 placeholder="Sökterm"
-									 aria-label="Sökterm"
-									 aria-describedby="basic-addon1"
-									 autoFocus
-									 onChange={this.props.onKeyPressed} />
-					</div>
-        )
-    }
+  render() {
+    return (
+        <div className="input-group mb-12">
+          <div className="input-group-prepend">
+              <span className="input-group-text"
+                    id="basic-addon1" role="img"
+                    aria-label="search">
+                      🔎
+              </span>
+          </div>
+          <input type="text"
+                 className="form-control"
+                 placeholder="Sökterm"
+                 aria-label="Sökterm"
+                 aria-describedby="basic-addon1"
+                 autoFocus
+                 onChange={this.props.onKeyPressed}/>
+        </div>
+    )
+  }
 }
 
 export default SearchBar;
